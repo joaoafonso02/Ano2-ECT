6afd3af9c481233d27a091fe50bd753d9100e35e
João Afonso <ferreiraafonsojoao@ua.pt>
2021-12-03 00:51:55 +0000

    easier way

--- AC1/TesteTreino/ex2.asm
+++ AC1/TesteTreino/ex2.asm
@@ -1,60 +1,64 @@
 # i: $t0
-# val: $t1
-# &(val[0]): $t3
+# v: $t1
+# &val[0]: $t2 = val
 
 	.data
 	.eqv SIZE, 8
+	.eqv print_string, 4
 	.eqv print_int10, 1
 	.eqv print_char, 11
-	.eqv print_string, 4
 	
 val:	.word 8,4,15,-1987,327,-9,27,16
-str1:	.asciiz "\nResult is: "
-
+str2:	.asciiz "\nResult is: "
+	
 	.text
 	.globl main
 	
 main:
+	la $t1, val 		# $t1 = val = &val[0]
 	li $t0, 0		# i = 0
-	la $t3, val		# $t3 = &(val[0])
 	
 do:
 	sll $t4, $t0, 2		# i * 4
-	addu $t2, $t4, $t3	# &(val[i])
+	addu $t4, $t4, $t1	# &array[i]
 	
-	lw $t4, 0($t2)		# $t4 = val[i]
-	lw $t5, 16($t2)		# $t5 = val[i + SIZE/2]
+	lw $t5, 0($t4)		# array[i]
+	lw $t6, 16($t4)		# array[i + SIZE/2]
 	
-	sw $t4, 16($t2)		# val[i] = val[i + SIZE/2]
-	sw $t5, 0($t2)		# val[i + SIZE/2] = val[i]
+	sw $t5, 16($t4)		# array[i] = array[i + SIZE/2]
+	sw $t6, 0($t4)		# array[i + SIZE/2] = array[i]
 	
 while:
-	addi $t0, $t0, 1	# ++i
-	bge $t0, 4, result	# while(++i < SIZE / 2)
+	addi $t0, $t0, 1	# i++
+	li $t3, SIZE		# $t3 = SIZE
+	div $t3, $t3, 2		# SIZE / 2
+	
+	bge $t0, $t3, write
 	j do
 	
-result:
-	la $a0, str1
+write:
+	la $a0, str2
 	li $v0, print_string
-	syscall			# print_string("Result is: ")
-	
-	li $t0, 0		# i = 0
-do2:
-	addu $a0, $t3, $t0
-	
-	lw $a0, 0($a0)
-	li $v0, print_int10
-	syscall			# print_int10(val[i]);
-	
-	addi $t0,$t0,4		# i++;
-	
-	li $a0, ','
-	li $v0, print_char	# print_char(',');
 	syscall
 	
-	li $a0, SIZE
-	sll $a0, $a0, 2
+	li $t0, 0		# i = 0
 	
-	blt $t0, $a0, do2	# } while( i < SIZE);
-		
+do2:
+	sll $t4, $t0, 2
+	addu $t4, $t4, $t1
+	
+	lw $a0, 0($t4)
+	li $v0, print_int10
+	syscall
+	
+	addi $t0, $t0, 1	# i++
+	
+	li $a0, ','
+	li $v0, print_char
+	syscall
+while2:
+	bge $t0, SIZE, endw
+	j do2
+	
+endw:
 	jr $ra
