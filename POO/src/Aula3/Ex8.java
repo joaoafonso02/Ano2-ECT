a8370363fd806a4dadbe48d78373ae4e980acd83
João Afonso <ferreiraafonsojoao@ua.pt>
2022-03-19 11:24:39 +0000

    aula3 done

--- POO/src/Aula3/Ex8.java
+++ POO/src/Aula3/Ex8.java
@@ -8,7 +8,7 @@ public class Ex8 {
         // variables
         double tgrade = 0, pgrade = 0;
         double[][] CLASS = new double[16][3];
-        String pauta = "";
+        //String pauta = "";
         // Filling the gaps
         for (int i = 0; i < CLASS.length; i++) {
             CLASS[i][0]= (int)(20.0 * Math.random()) + 1;
@@ -18,28 +18,21 @@ public class Ex8 {
             pgrade = CLASS[i][1];
 
             CLASS[i][2] = (int) Math.round(0.4 * tgrade + 0.6 * pgrade);
-            if(CLASS[i][2] < 9.5) {
-                    pauta = RED + CLASS[i][2] + NORMAL;
-            } else 
-                pauta = NORMAL +CLASS[1][2];
+            //if(CLASS[i][2] < 9.5) {
+             //       pauta = RED + CLASS[i][2] + NORMAL;
+            //} else 
+             //   pauta = NORMAL +CLASS[1][2];
             
         }
         // Print Grade Table
         System.out.println(GREEN + "NotaT   NotaP   Pauta" + NORMAL);
         for (int i = 0; i < CLASS.length; ++i) {
             for(int j = 0; j < CLASS[i].length; ++j) {
-                //if(CLASS[i][2] < 9.5) {
-                //    System.out.println(RED + CLASS[i][2] + NORMAL);
-                //} else 
                 System.out.printf(String.format("%4.1f", CLASS[i][j]) + "    "); 
             }
             System.out.println();
         }
 
         sc.close();
-    }
-
-    private static String StringValueOf(double d) {
-        return null;
     }
 }
