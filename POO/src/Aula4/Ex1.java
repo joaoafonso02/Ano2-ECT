bf6d33ef0cc7ced8ec8eb113d2453bc828018531
João Afonso <ferreiraafonsojoao@ua.pt>
2022-03-26 12:19:33 +0000

    Aula4 - DONE POO

--- POO/src/Aula4/Ex1.java
+++ POO/src/Aula4/Ex1.java
@@ -2,8 +2,7 @@ package Aula4;
 import java.util.*;
 
 public class Ex1 {
-    public static  String NORMAL = "\033[0m"; public static String RED = "\033[0;31m";
-     public static String ANSI_PURPLE = "\u001B[36m";    public static void main(String[] args) {
+    public static  String NORMAL = "\033[0m"; public static String RED = "\033[0;31m"; public static String ANSI_PURPLE = "\u001B[36m";    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sentence;
         do {
@@ -19,6 +18,7 @@ public class Ex1 {
             System.out.printf("\nUpper case converted String: %s", sentence.toUpperCase());
             System.out.printf("\nLast char: %s", sentence.charAt(sentence.length()-1));
             System.out.printf("\nFirst 3 chars: %s", sentence.substring(0,3));
+            System.out.printf("\nLast 3 chars: %s", sentence.substring(sentence.length() - 3));
             System.out.printf("\nYour sentence concatenated: %s", sentence.concat("! Goodbye!"));
             System.out.printf("\nThe char r replaced by l: %s", sentence.replace("l", "r"));
             System.out.printf("\nYour sentence contains the world --> |World|: %s", sentence.contains("World"));
